c337ba4db09fc7e7b854162f8f963bfffa979e66
prince1565 <kumar.prince832002@gmail.com>
2024-08-13 17:15:17 +0530

    skill update

--- src/components/Skillset/Techstack.jsx
+++ src/components/Skillset/Techstack.jsx
@@ -4,15 +4,14 @@ import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiGit,
+ 
 } from "react-icons/di";
 import {
- 
-  SiGraphql,
-  SiSolidity,
   SiRedux,
-  SiMongodb,
-  SiHtml5
+ SiMysql,
+  SiExpress,
+  SiHtml5,
+  SiCss3
 } from "react-icons/si";
 
 const Techstack = () => {
@@ -22,25 +21,30 @@ const Techstack = () => {
         <SiHtml5 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <SiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
+        <SiExpress />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiRedux />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMongodb />
+        <SiMysql />
       </Col>
       
       
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
+      
     </Row>
   );
 }
--- src/components/Skillset/Toolstack.jsx
+++ src/components/Skillset/Toolstack.jsx
@@ -3,20 +3,30 @@ import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
-  SiHeroku,
-  SiVercel,
-  SiNetlify
+  
 } from "react-icons/si";
+import {
+  DiGit,
+}from "react-icons/di";
+
+import { FaGithub } from "react-icons/fa";
 
 const Toolstack = () => {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaGithub />
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
       </Col>
+
       
       
       
